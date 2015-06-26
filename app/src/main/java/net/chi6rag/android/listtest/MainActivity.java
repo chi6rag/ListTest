031709320a87947dd23c2af16423c53ca606c446
Chirag Aggarwal <chi6rag@gmail.com>
2015-06-26 10:32:30 +0530

    Setup Custom view in ArrayAdapter

--- app/src/main/java/net/chi6rag/android/listtest/MainActivity.java
+++ app/src/main/java/net/chi6rag/android/listtest/MainActivity.java
@@ -1,31 +1,21 @@
 package net.chi6rag.android.listtest;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-public class MainActivity extends ListActivity{
+public class MainActivity extends Activity {
     String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-
     ListView l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        l = getListView();
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, days);
-        l.setAdapter(arrayAdapter);
-    }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        TextView textView = (TextView) v;
-        Log.d("chi6rag", textView.getText().toString());
+        l = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                R.layout.single_row, R.id.textView, days);
+        l.setAdapter(arrayAdapter);
     }
 }
