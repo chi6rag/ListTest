5b86a257d5bdbe92076a50c9b4a65b786d52ed78
Chirag Aggarwal <chi6rag@gmail.com>
2015-06-26 09:57:57 +0530

    Add ListActivity way of handling LayoutAdapters

--- app/src/main/java/net/chi6rag/android/listtest/MainActivity.java
+++ app/src/main/java/net/chi6rag/android/listtest/MainActivity.java
@@ -1,36 +1,31 @@
 package net.chi6rag.android.listtest;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-
-public class MainActivity extends ActionBarActivity implements OnItemClickListener {
+public class MainActivity extends ListActivity{
+    String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
     ListView l;
-    String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-            "Saturday", "Sunday"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        l = (ListView) findViewById(R.id.listView);
+        l = getListView();
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,days);
+                android.R.layout.simple_list_item_1, days);
         l.setAdapter(arrayAdapter);
-        l.setOnItemClickListener(this);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        TextView textView = (TextView) view;
-        Log.d("chi6rag", (String) textView.getText());
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        TextView textView = (TextView) v;
+        Log.d("chi6rag", textView.getText().toString());
     }
 }
